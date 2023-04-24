f40bcba066e36cd5da31f0b84959a6875c6c7c24
paciadawid <paciadawid@gmail.com>
2023-04-24 18:30:45 +0200

    gorest user creation

--- src/gorest_handler.py
+++ src/gorest_handler.py
@@ -2,14 +2,18 @@ import requests
 
 
 class APIHandler:
-
     base_url = "https://gorest.co.in/public/v2"
     user_endpoint = "/users"
     headers = {
         "Authorization": "Bearer 56c3f5739c2af4edb091c270360c0d22f7f804005a9b598801b24ee6b3e45f8f"
     }
 
-    def create_user(self, body):
-        res = requests.post(self.base_url+self.user_endpoint, json=body, headers=self.headers)
-        assert res.status_code == 201
+    def create_user(self, body, expected_status_code=201):
+        res = requests.post(self.base_url + self.user_endpoint, json=body, headers=self.headers)
+        assert res.status_code == expected_status_code
+        return res.json()
+
+    def get_user_by_id(self, user_id, expected_status_code=200):
+        res = requests.get(f"{self.base_url}{self.user_endpoint}/{user_id}", headers=self.headers)
+        assert res.status_code == expected_status_code
         return res.json()
--- tests/gorest_test.py
+++ tests/gorest_test.py
@@ -1,5 +1,4 @@
 import unittest
-import requests
 from faker import Faker
 from src.gorest_handler import APIHandler
 
@@ -18,8 +17,17 @@ class GorestTest(unittest.TestCase):
         }
         res_body = self.api_handler.create_user(body)
         self.assertEqual(int, type(res_body["id"]))
+        res_body = self.api_handler.get_user_by_id(res_body["id"])
         self.assertDictContainsSubset(body, res_body)
 
+    def test_invalid_user_creation(self):
+        body = {
+            "name": Faker().name(),
+            "gender": "male",
+            "email": Faker().name(),
+            "status": "active"
+        }
+        self.api_handler.create_user(body, expected_status_code=422)
 
 if __name__ == '__main__':
     unittest.main()
