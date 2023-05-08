7228a3bc5ebf5441ccdfbfd5fd733783d3478a64
paciadawid <paciadawid@gmail.com>
2023-05-08 09:29:27 -0700

    posts tests

--- src/gorest_handler.py
+++ src/gorest_handler.py
@@ -1,14 +1,23 @@
 import requests
+from faker import Faker
 
 
 class APIHandler:
     base_url = "https://gorest.co.in/public/v2"
     user_endpoint = "/users"
+    post_endpoint = "/posts"
     headers = {
         "Authorization": "Bearer 56c3f5739c2af4edb091c270360c0d22f7f804005a9b598801b24ee6b3e45f8f"
     }
 
-    def create_user(self, body, expected_status_code=201):
+    def create_user(self, body=None, expected_status_code=201, random_user=False):
+        if random_user:
+            body = {
+                "name": Faker().name(),
+                "gender": "male",
+                "email": Faker().email(),
+                "status": "active"
+            }
         res = requests.post(self.base_url + self.user_endpoint, json=body, headers=self.headers)
         assert res.status_code == expected_status_code
         return res.json()
@@ -23,3 +32,17 @@ class APIHandler:
 
     def delete_user(self, user_id):
         pass
+
+    def create_post(self, user_id, post_body, expected_status_code=201):
+        res = requests.post(f"{self.base_url}{self.user_endpoint}/{user_id}{self.post_endpoint}", json=post_body, headers=self.headers)
+        assert res.status_code == expected_status_code
+        return res.json()
+
+    def get_post(self, post_id, expected_status_code=200):
+        res = requests.get(f"{self.base_url}{self.post_endpoint}/{post_id}", headers=self.headers)
+        assert res.status_code == expected_status_code
+        return res.json()
+
+    def delete_post(self, post_id):
+        res = requests.delete(f"{self.base_url}{self.post_endpoint}/{post_id}", headers=self.headers)
+        assert res.status_code == 204
--- tests/gorest_test.py
+++ tests/gorest_test.py
@@ -29,14 +29,33 @@ class GorestTest(unittest.TestCase):
         }
         self.api_handler.create_user(body, expected_status_code=422)
 
-    def test_e2e_flow(self):
-        self.api_handler.create_user()
-        self.api_handler.get_user_by_id()
-        self.api_handler.update_user()
-        self.api_handler.get_user_by_id()
-        self.api_handler.delete_user()
-        self.api_handler.get_user_by_id()
+    # def test_e2e_flow(self):
+    #     self.api_handler.create_user()
+    #     self.api_handler.get_user_by_id()
+    #     self.api_handler.update_user()
+    #     self.api_handler.get_user_by_id()
+    #     self.api_handler.delete_user()
+    #     self.api_handler.get_user_by_id()
 
+    def test_add_delete_post(self):
+        user_id = self.api_handler.create_user(random_user=True)["id"]
+        post_body = {
+            "title": Faker().sentence(),
+            "body": Faker().paragraph()
+        }
+        post_id = self.api_handler.create_post(user_id, post_body)["id"]
+        user_post = self.api_handler.get_post(post_id)
+        self.assertTrue(user_post)
+        self.api_handler.delete_post(post_id)
+        self.api_handler.get_post(post_id, expected_status_code=404)
+
+    def test_create_post_without_body(self):
+        user_id = self.api_handler.create_user(random_user=True)["id"]
+        post_body = {
+            "title": Faker().sentence()
+        }
+        res_body = self.api_handler.create_post(user_id, post_body, expected_status_code=422)
+        self.assertEqual("body", res_body[0]["field"])
 
 if __name__ == '__main__':
     unittest.main()
